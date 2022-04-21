c325ae2a81c14ba3fcb9030cfdcaec1a8f6eb0fb
Franklin Shera <fshera96@gmail.com>
2022-04-21 23:50:16 +0300

    Cards Component

---
+++ app/View/Components/GenieCards.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\View\Components;
+
+use Illuminate\View\Component;
+
+class GenieCards extends Component
+{
+    public $feeds;
+    public $cardType;
+    public $section;
+    public $title;
+    public $limit;
+
+    public $types = ['SMALL' => 'small-card', 'BIG' => 'big-card', 'TOP_HALF_BIG' => 'half-big-card'];
+    /**
+     * Create a new component instance.
+     *
+     * @return void
+     */
+    public function __construct($feeds = [], $cardType = "SMALL", $section = "", $title = "", $limit = 8)
+    {
+        $this->feeds = $feeds;
+        $this->cardType = $this->types[$cardType];
+        $this->section = $section;
+        $this->title = $title;
+        $this->limit = $limit;
+
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     *
+     * @return \Illuminate\Contracts\View\View|\Closure|string
+     */
+    public function render()
+    {
+        return view('components.genie-cards');
+    }
+}
