90b02c147cc71f11fc4aecd0b1a8dc1a904d5d40
Franklin Shera <fshera96@gmail.com>
2022-06-27 11:06:07 +0300

    Post Action : Create Post

---
+++ app/Actions/Post/CreatePost.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Actions\Post;
+
+use App\Models\Post;
+use Illuminate\Support\Facades\Auth;
+
+class CreatePost
+{
+    /**
+     * Creates New Post
+     *
+     * @param  array  $postData
+     * @return void
+     */
+    public function create(array $postData)
+    {
+
+        Post::create([
+            'title' => $postData['title'],
+            'slug' => $postData['title'],
+            'user_id' => Auth::user()->id,
+            'richtext' => $postData['quillcontent'],
+            'content' => (new RenderQuillContent())->render($postData['quillcontent']),
+        ]);
+    }
+}
