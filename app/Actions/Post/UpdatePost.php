97a1d0caf59f5fc62af502cf1acd18273ab9467f
Franklin Shera <fshera96@gmail.com>
2022-06-27 11:29:26 +0300

    Post Action : Post Update Action

---
+++ app/Actions/Post/UpdatePost.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Actions\Post;
+
+use App\Models\Post;
+
+class UpdatePost
+{
+    /**
+     * Updates Post
+     *
+     * @param  App\Model\Post  $post
+     * @param  array  $postData
+     * @return void
+     */
+    public function update(Post $post, array $postData)
+    {
+
+        $post->update([
+            'title' => $postData['title'],
+            'slug' => $postData['title'],
+            'richtext' => $postData['quillcontent'],
+            'content' => (new RenderQuillContent())->render($postData['quillcontent']),
+        ]);
+    }
+}
